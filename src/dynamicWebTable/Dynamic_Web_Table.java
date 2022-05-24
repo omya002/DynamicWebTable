abc6e2449a63290c96027fd2519b594642f6acd3
omya002 <bagadeom002@gmail.com>
2022-05-24 12:20:37 +0530

    DynamicWebTableCode

---
+++ src/dynamicWebTable/Dynamic_Web_Table.java
@@ -0,0 +1,42 @@
+package dynamicWebTable;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dynamic_Web_Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\Selenium\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://cosmocode.io/automation-practice-webtable/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+
+		List<WebElement> totalRows =  driver.findElements(By.xpath("//table[@id ='countries']//tbody//tr"));
+		System.out.println("Total Rows in the table are : " +totalRows.size());
+
+		List<WebElement> totalColumns = driver.findElements(By.xpath("//table[@id ='countries']//tbody//tr//h3"));
+		System.out.println("Total Columns in the table are : " +totalColumns.size());
+
+		String expectedCurrency = "Euro";
+
+		for(int i=1;i<=totalRows.size();i++) {
+			for(int j=1;j<=totalColumns.size(); j++) {
+				if(driver.findElement(By.xpath("//table[@id ='countries']//tbody//tr["+i+"]//td["+j+"]")).getText().equalsIgnoreCase(expectedCurrency)) {
+					System.out.println(driver.findElement(By.xpath("//table[@id ='countries']//tbody//tr["+i+"]//td["+j+"]")).getText());
+					break;
+				}	
+			}
+		}
+		driver.quit();
+	}
+
+}
